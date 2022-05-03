2964d31167b7895cac6ababa44331ba5ca16cd0a
harry-tomlins <92577647+harry-tomlins@users.noreply.github.com>
2022-05-03 09:57:33 +0100

    Update Velocities.s

--- Velocities.s
+++ Velocities.s
@@ -15,13 +15,13 @@ psect	Velocities, class=CODE
 input_setup:
     
     movlw   0
-    movwf   paddle_L_v,A
+    movwf   paddle_L_v,A ;reset paddle velocities
     movwf   paddle_R_v,A
     call randvel
     movlw   0x0F
-    movwf   TRISE, A
+    movwf   TRISE, A ;setting up as input for buttons
     movlw   0xFF
-    movwf   TRISJ, A
+    movwf   TRISJ, A ;setting up as input for buttons
     return
     
 randvel:
@@ -32,50 +32,51 @@ randvel:
 
 paddle_L_check:
     movlw   1
-    subwf   PORTE, W, A
+    subwf   PORTE, W, A ;checks if down button being pressed
     bz	downspeedL
     movlw   8
-    subwf   PORTE, W, A
+    subwf   PORTE, W, A ;checks if up button being pressed
     bz	upspeedL
     
-nospeedL:
+nospeedL: ;called if no buttons or two buttons being pressed
     movlw   0
-    movwf   paddle_L_v,A
+    movwf   paddle_L_v,A ;sets paddle velocity to 0
     return
 
 downspeedL:
     movlw   2
-    movwf   paddle_L_v,A
+    movwf   paddle_L_v,A ;sets paddle velocity to 2
     return
 
 upspeedL:
     movlw   -2
-    movwf   paddle_L_v,A
+    movwf   paddle_L_v,A ;sets paddle velocity to -2
     return
 
 paddle_R_check:
     movlw   1
-    subwf   PORTJ, W, A
+    subwf   PORTJ, W, A ;checks if down button being pressed
     bz	downspeedR
     movlw   8
-    subwf   PORTJ, W, A
+    subwf   PORTJ, W, A ;checks if up button being pressed
     bz	upspeedR
-nospeedR:
+    
+nospeedR: ;if no buttons or two buttons were pressed code comes here
     movlw   0
-    movwf   paddle_R_v,A
+    movwf   paddle_R_v,A ;sets paddle velocity to 0
     return
 
 downspeedR:
     movlw   2
-    movwf   paddle_R_v,A
+    movwf   paddle_R_v,A ;sets paddle velocity to 2
     return
 
 upspeedR:
     movlw   -2
-    movwf   paddle_R_v,A
+    movwf   paddle_R_v,A ;sets paddle velocity to 2
     return
 
-pos_update:
+pos_update: ;adds the paddle velocities to the positions to find position of the paddle at next frame
     movf    paddle_L_v, W, A
     addwf   paddle_L_x, F, A
     bcf	    paddle_L_x, 6, A
@@ -86,8 +87,8 @@ pos_update:
     bcf	    paddle_R_x, 7, A
     movf    ball_v_x, W, A
     addwf   ball_x, F, A
-    bcf	    ball_x, 7, A
+    bcf	    ball_x, 7, A ;clear the 7th bit so dont get an overflow above allowed coordinates
     movf    ball_v_y, W, A
     addwf   ball_y, F, A
-    bcf	    ball_y, 7, A
+    bcf	    ball_y, 7, A ;clear the 7th bit so dont get an overflow above allowed coordinates
     return
