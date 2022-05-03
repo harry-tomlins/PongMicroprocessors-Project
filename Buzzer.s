d27991b85f48ff4615a876b3a617cc61ad25764b
harry-tomlins <92577647+harry-tomlins@users.noreply.github.com>
2022-05-03 09:28:56 +0100

    Update Buzzer.s

--- Buzzer.s
+++ Buzzer.s
@@ -9,13 +9,13 @@ psect  buzzer_code, class=CODE
     
     
 buzzer:
-    bcf	PORTB, 6, A	    ;set B6 pin high
+    bcf	PORTB, 6, A	    ;set B6 pin low
     btfss   buzz_check,0,A
     goto game_loop
     movlw 1
-    call LCD_delay_ms
-    bsf	PORTB, 6, A		
-    btfss buzz_check,0,A
+    call LCD_delay_ms ;set the frequency of buzzer to 500Hz by having 2ms time period
+    bsf	PORTB, 6, A		;set B6 pin high
+    btfss buzz_check,0,A ;if buzz_check has been toggeled from within the interupt then branch back to main code
     goto game_loop
     movlw 1
     call LCD_delay_ms
